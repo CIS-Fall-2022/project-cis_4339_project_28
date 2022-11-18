0a22dbf717c0c5be470ebd669afad8e7d1c1977a
ashtonchiang <77808315+ashtonchiang@users.noreply.github.com>
2022-11-18 14:37:31 -0600

    Update findClient.vue

--- frontend/src/components/findClient.vue
+++ frontend/src/components/findClient.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div>
-      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Find Client</h1>
+      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Find a Client</h1>
     </div>
     <div class="px-10 pt-20">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
@@ -72,7 +72,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
       <div class="ml-10">
         <h2 class="text-2xl font-bold">List of Clients</h2>
-        <h3 class="italic">Click table row to edit/display an entry</h3>
+        <h3 class="italic">Click table row to edit/view an entry</h3>
       </div>
       <div class="flex flex-col col-span-2">
         <table class="min-w-full shadow-md rounded">
